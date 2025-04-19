da1e05b06da8b753525b8e3b2c7500cda643b858
wikipop <66514885+wikipop@users.noreply.github.com>
2025-04-19 20:09:00 +0200

    fix: proper method for custom speech saving

--- utils/audio/generate_voice.py
+++ utils/audio/generate_voice.py
@@ -25,20 +25,24 @@ def generate_speech_from_text(text: str) -> AudioSegment:
     Returns:
         BinaryIO: A binary stream containing the generated MP3 audio.
     """
-    client = ElevenLabs(api_key=os.environ["ELEVEN_LABS_API_KEY"])
-
     cache = Cache(cache_dir="cache")
+
+    if cached_mp3 := cache.is_custom_mp3_cached(text):
+        logging.info(f"Using cached MP3 for {text} - by hash")
+        return cached_mp3 + 6
+
     text_hash = hashlib.sha256(text.encode("utf-8")).hexdigest()
     if cached_mp3 := cache.is_custom_mp3_cached(text_hash):
         logging.info(f"Using cached MP3 for {text_hash}")
         return cached_mp3 + 6
 
     logging.info(f"Generating MP3 for {text_hash}")
+    client = ElevenLabs(api_key=os.environ["ELEVEN_LABS_API_KEY"])
     audio = b"".join(client.text_to_speech.convert(
         text=text, voice_id="pqHfZKP75CvOlQylNhV4", model_id="eleven_flash_v2_5",
         language_code="pl", output_format="mp3_44100_128", ))
 
     logging.info(f"Saving MP3 for {text_hash}")
-    cache.save_mp3(audio, "custom", text_hash)
+    cache.save_custom_mp3(audio, text_hash)
 
     return cache.load_custom_mp3(text_hash) + 6
