de3e543a714f96c081cd28af5df58ac85663c1a5
Wojciech Piech <ilikeahottodogu@gmail.com>
2025-04-21 17:53:15 +0200

    Use Pathlib abstraction over typed path strings

--- entities/cache/local_cache.py
+++ entities/cache/local_cache.py
@@ -10,7 +10,7 @@ from entities.catalogue.category import Category
 
 class LocalCache(ICache):
     def __init__(self, cache_dir):
-        super().__init__(cache_dir)
+        super().__init__(pathlib.Path(cache_dir))
 
     @staticmethod
     def _map_category_to_string(category: Category | str = None):
@@ -34,29 +34,29 @@ class LocalCache(ICache):
     def generate_category_directory(self, category: Category | str = None) -> None:
         category_dir = self._map_category_to_string(category)
 
-        if not pathlib.Path(f"./{self.cache_dir}/{category_dir}").exists():
-            pathlib.Path(f"./{self.cache_dir}/{category_dir}").mkdir()
+        if not self.cache_dir.joinpath(category_dir).exists():
+            self.cache_dir.joinpath(category_dir).mkdir()
 
     def is_mp3_cached(self, hash: str, category: Category | str = None) -> bool:
         category_dir = self._map_category_to_string(category)
 
-        return pathlib.Path(f"{self.cache_dir}/{category_dir}/{hash}.mp3").exists()
+        return self.cache_dir.joinpath(category_dir, f"{hash}.mp3").exists()
 
     def save_mp3(self, audio: bytes, hash: str, category: Category | str = None) -> None:
         category_dir = self._map_category_to_string(category)
 
         self.generate_category_directory(category)
 
-        with open(f"{self.cache_dir}/{category_dir}/{hash}.mp3", "wb") as f:
+        with open(self.cache_dir.joinpath(category_dir, f"{hash}.mp3"), "wb") as f:
             f.write(audio)
 
     def load_mp3(self, hash: str, category: Category | str = None) -> AudioSegment:
         category_dir = self._map_category_to_string(category)
 
-        return AudioSegment.from_mp3(f"{self.cache_dir}/{category_dir}/{hash}.mp3")
+        return AudioSegment.from_mp3(self.cache_dir.joinpath(category_dir, f"{hash}.mp3"))
 
     def load_random_mp3(self, category: Category | str = None) -> AudioSegment:
         category_dir = self._map_category_to_string(category)
-        list_of_files = list(pathlib.Path(f"{self.cache_dir}/{category_dir}").glob("*.mp3"))
+        list_of_files = list(self.cache_dir.joinpath(category_dir).glob("*.mp3"))
 
         return AudioSegment.from_mp3(random.choice(list_of_files))
